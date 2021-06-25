d30480cc3e9e35cffa3fdc47ccbfe3cfaf8dc713
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-25 17:27:23 +0200

    implemented image slider

--- js/script.js
+++ js/script.js
@@ -5,17 +5,33 @@ new Vue(
         el: '#app',
         data: {
             landscapeImages: [
-                '../img/img-1.jpg',
-                '../img/img-2.jpg',
-                '../img/img-3.jpg',
-                '../img/img-4.jpg'
-            ]
+                'img/img-1.jpg',
+                'img/img-2.jpg',
+                'img/img-3.jpg',
+                'img/img-4.jpg'
+            ],
+            sliderIndex: 0
         },
         created() {
-
+            setInterval(() => {
+                this.next();
+            }, 3000)
         },
         methods: {
-            
+            next: function() {
+                if (this.sliderIndex === (this.landscapeImages.length - 1)) {
+                    this.sliderIndex = 0;
+                } else {
+                    this.sliderIndex++;
+                }
+            },
+            prev: function() {
+                if (this.sliderIndex === 0) {
+                    this.sliderIndex = (this.landscapeImages.length - 1);
+                } else {
+                    this.sliderIndex--;
+                }
+            }
         }
     }
 )
