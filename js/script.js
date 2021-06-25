3f694fc957eea0a8aed944e5dd45465a6d7e465a
mattia-dev <82512569+mattia-dev@users.noreply.github.com>
2021-06-25 16:53:04 +0200

    created html, css and js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            landscapeImages: [
+                '../img/img-1.jpg',
+                '../img/img-2.jpg',
+                '../img/img-3.jpg',
+                '../img/img-4.jpg'
+            ]
+        },
+        created() {
+
+        },
+        methods: {
+            
+        }
+    }
+)
